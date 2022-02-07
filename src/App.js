1ebd44bcbb2328b9d24a93490c01763b8ae454a0
Afiz Savage <savageafiz9@gmail.com>
2022-02-07 13:57:29 +0000

    Create routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
 
-const App = () => (<div>AAAAA</div>);
+import './App.css';
+import Details from './components/Details';
+import Home from './components/Home';
+
+const App = () => (
+  <>
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route path="/details" element={<Details />} />
+    </Routes>
+  </>
+);
 
 export default App;
